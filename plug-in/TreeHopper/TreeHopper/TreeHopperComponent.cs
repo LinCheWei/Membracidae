e4683cb04e78cb2f3ed2c6ccba88f6cb25582a9c
LinCheWei <91247905+LinCheWei@users.noreply.github.com>
2023-07-22 20:54:28 +0200

    added treehopper plug-in

---
+++ plug-in/TreeHopper/TreeHopper/TreeHopperComponent.cs
@@ -0,0 +1,171 @@
+using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Xml;
+using LibGit2Sharp;
+using System.Drawing;
+using Rhino.UI;
+
+
+namespace TreeHopper
+{
+    public class TreeHopperComponent : GH_Component
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public TreeHopperComponent()
+          : base("TreeHopper", "THZ",
+            "test component",
+            "Treehopper", "Subcategory")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("open", "open", "open git repo", GH_ParamAccess.item, false);
+            pManager.AddTextParameter("version", "vers", "dadsad", GH_ParamAccess.list, new List<string>() {"0"});
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("output", "output", "test", GH_ParamAccess.item);
+        }
+
+        bool open;
+
+
+        private void terminationCallback(GH_Document doc)
+        {
+            /// Remove created valueList
+            List<IGH_DocumentObject> toDelete = new List<IGH_DocumentObject>();
+            foreach (IGH_DocumentObject comp in doc.Objects)
+            {
+                if (comp.Description == "Treehopper_valuelist")
+                {
+                    toDelete.Add(comp);
+                }
+            }
+            if (toDelete.Count > 0)
+            {
+                doc.RemoveObjects(toDelete, true);
+            }
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            List<string> diffID = new List<string>();
+            DA.GetData(0, ref open);
+            GH_Document doc = Grasshopper.Instances.ActiveCanvas.Document;
+            string filename = System.IO.Path.GetFileName(doc.FilePath);
+            string path = System.IO.Path.GetDirectoryName(doc.FilePath);
+            List<Tree> targets = new List<Tree>(); 
+            List<string> versionId = new List<string>();
+
+            DA.SetData(0, path);
+
+
+            if (open)
+            {
+                using (var repo = new Repository(path))
+                {
+                    string message = "";
+                    var fileHistory = repo.Commits.QueryBy(filename, new CommitFilter { SortBy = CommitSortStrategies.Time }); /// search through commit that contains the changes of this ghx
+
+                    /// Acquire commit info
+                    foreach (LogEntry e in fileHistory)
+                    {
+                        message += string.Format("id:{0}, author:{1}\r\n", e.Commit.Id, e.Commit.Author);
+                        targets.Add(e.Commit.Tree);
+                        foreach (TreeEntry te in e.Commit.Tree)
+                        {
+                            if (te.Name == filename)
+                            {
+                                message += te.Target.Sha.Substring(0, 7) + "\r\n";
+                                versionId.Add(te.Target.Sha.Substring(0, 7));
+                            }
+                        }
+                    }
+
+
+                    /// Create value list
+                    bool createVallist = true;
+                    foreach (IGH_DocumentObject comp in doc.Objects)
+                    {
+                        if (comp.Description == "Treehopper_valuelist")
+                        {
+                            createVallist = false;
+                        }
+                    }
+                    if (createVallist)
+                    {
+                        var valList = new Grasshopper.Kernel.Special.GH_ValueList();
+                        valList.CreateAttributes();
+                        valList.Name = "Versions";
+                        valList.NickName = "vers";
+                        valList.Description = "Treehopper_valuelist";
+                        valList.ListMode = Grasshopper.Kernel.Special.GH_ValueListMode.DropDown;
+
+                        valList.Attributes.Pivot = new PointF((float)this.Attributes.DocObject.Attributes.Bounds.Left - valList.Attributes.Bounds.Width - 15,
+                            (float)this.Params.Input[1].Attributes.Bounds.Y);
+
+                        valList.ListItems.Clear();
+
+                        for (int i = 0; i < targets.Count; i++)
+                        {
+                            valList.ListItems.Add(new Grasshopper.Kernel.Special.GH_ValueListItem(versionId[i], i.ToString()));
+                        }
+
+                        doc.AddObject(valList, false);
+                        this.Params.Input[1].RemoveAllSources();
+                        this.Params.Input[1].AddSource(valList);
+                        valList.ExpireSolution(true);
+                    }
+                    DA.GetDataList(1, diffID);
+
+                    List<string> tt = new List<string>();
+                    tt.Add(filename);
+
+                    Patch difference = repo.Diff.Compare<Patch>(targets[int.Parse(diffID[0])], DiffTargets.WorkingDirectory, tt);
+                    DA.SetData(0, difference.Content);
+                }
+            }
+            else
+            {
+                doc.ScheduleSolution(5, terminationCallback);
+            }
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// You can add image files to your project resources and access them like this:
+        /// return Resources.IconForThisComponent;
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => null;
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("ba896193-fe68-46f0-b568-796c88675d8a");
+    }
+}
---
+++ plug-in/TreeHopper/TreeHopper/TreeHopperInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace TreeHopper
+{
+    public class TreeHopperInfo : GH_AssemblyInfo
+    {
+        public override string Name => "TreeHopper";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("8319f7db-0762-4839-835e-f5cfb3bb117c");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
